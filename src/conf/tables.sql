a06a9a71e4e523ed8ce312f0af6ffb481e97f785
Joachim <joachimvalgren@gmail.com>
2014-03-18 12:09:36 +0100

    Stuff

--- src/conf/tables.sql
+++ src/conf/tables.sql
@@ -9,7 +9,7 @@ postalDistrict varchar(40)
 );
 
 create table Persons (
-cpr int primary key,
+cpr varchar(11) primary key,
 title varchar(80),
 firstName varchar(80) not null,
 lastName varchar(80) not null,
@@ -24,12 +24,12 @@ create table Account (
 accountNumber varchar(80) primary key,
 balance int,
 interest decimal,
-owner int not null references Persons(cpr)
+owner varchar(11) not null references Persons(cpr)
 );
 
 create table Transfer (
 amount decimal,
 transferDate date,
-sourceAccount varchar(80) not null references Account(accountNumber),
+sourceAccount varchar(80) primary key not null references Account(accountNumber),
 targetAccount varchar(80) not null references Account(accountNumber)
 );
--- src/conf/values.sql
+++ src/conf/values.sql
@@ -1,10 +1,24 @@
-insert into Postal values (4550, 'Greve');
-insert into Postal values (7777, 'Baron');
-insert into Postal values (7007, 'Ølstykke');
-
-insert into Persons values (1212560555, 'Hr', 'Kurt', 'Hansen', 'Solvej 8', 4550,
-'01234567', 'kurt@mail.dk', null);
-insert into Persons values (1111900444, 'Fr', 'Sonja', 'Jensen', 'Månegade 8', 7777,
-'76543210', 'sonja@mail.dk', null);
-insert into Persons values (0102560777, 'Hr', 'James', 'Bond', 'Mars Alle 17', 7007,
-'20007007', 'james@bond.dk', null);
+insert into Persons values ('121256-0555', 'Hr', 'Kurt', 'Hansen', 'Solvej 8', 4550,
+'01234567', 'kurt@mail.dk', 'test');
+insert into Persons values ('111190-0444', 'Fr', 'Sonja', 'Jensen', 'Månegade 8', 8000,
+'76543210', 'sonja@mail.dk', 'test');
+insert into Persons values ('010256-0777', 'Hr', 'James', 'Bond', 'Mars Alle 17', 7007,
+'20007007', 'james@bond.dk', 'test');
+insert into Persons values ('1305902222','Hr','Hans','Hansen','Balevej 9',4550,'42424252','Hans@email.dk',null);
+insert into Persons values ('1405903333','Hr','Jan','Jansen','JansenVej 10',2700,'67262819','Jan@email.dk',null);
+insert into Persons values ('1510442323','Fr','Julie','Julisen','JuleVej 11',2800,'28329832','Julie@email.dk',null);
+insert into Persons values ('1209903232','Fr','Anna','Annasen','AndVej 13',3300,'75927191','Anne@email.dk',null);
+insert into Persons values ('1406399999','Hr','Lars','Larsen','Landvejen 42',9200,'92327151','Lars@email.dk',null);
+insert into Persons values ('1204881231','Fr','Marie','Mariesen','MarkenVej 32',9000,'93281791','Marie@email.dk',null);
+insert into Persons values ('2306878918','Hr','Kasper','Kaspersen','KarsenVej 33',6760,'75757291','Kasper@email.dk',null);
+insert into Persons values ('2303830990','Fr','Elisabeth','Elisaben','ElVejen 55',5700,'67869699','Eli@email.dk',null);
+insert into Persons values ('1208789990','Hr','Ulrik','Ulriksen','UdeBoenVej 75',2300,'65786891','Ulrik@email.dk',null);
+insert into Persons values ('1308893232','Fr','Bigitte','Bigittesen','Bagvejen 21',2100,'91238191','Bigitte@email.dk',null);
+insert into Persons values ('1508782312','Hr','Ivan','Ivansen','InternVej 91',8800,'56662781','Ivan@email.dk',null);
+insert into Persons values ('1109872121','Fr','Nina','Ninasen','NakkeVejen 12',5200,'93237519','Ninna@email.dk',null);
+insert into Persons values ('1608529091','Hr','Alexander','Alexsen','AlleGyden 11',4400,'27951781','Alex@email.dk',null);
+insert into Persons values ('1512741212','Fr','Vanessa','Vanessen','VandermarkVej 30',2600,'28588192','Vannessa@email.dk',null);
+insert into Persons values ('2310568877','Hr','Saleh','Salehsen','SamarmargeVej 11',2100,'75195218','Saleh@email.dk',null);
+insert into Persons values ('2109401212','Fr','Sille','Sillesen','SildeVejen 85',7900,'65231818','Sille@email.dk',null);
+insert into Persons values ('1205351231','Hr','David','Davidsen','DanmarkVej 99',2100,'85182819','David@email.dk',null);
+insert into Persons values ('2412453313','Fr','Janne','Jannesen','JakkeVej 78',4500,'12019320','Janne@email.dk',null);
